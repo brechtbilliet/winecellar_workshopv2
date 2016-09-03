3d9af4de09b15f30bfcd7c3228c743107b1fc308
Brecht Billiet <billietbrecht@gmail.com>
2016-09-03 11:27:33 +0200

    busy handling

---
+++ src/app/customHttp.ts
@@ -0,0 +1,111 @@
+import {Injectable} from "@angular/core";
+import {Http, RequestOptionsArgs, RequestOptions, ConnectionBackend, Request, Response} from "@angular/http";
+import {Observable} from "rxjs/Rx";
+import {Store} from "@ngrx/store";
+import {ApplicationState} from "../statemanagement/state/ApplicationState";
+import {
+    CONTAINER_APPLICATION_DISABLE_BUSY_FLAG,
+    CONTAINER_APPLICATION_ENABLE_BUSY_FLAG
+} from "../statemanagement/actionTypes";
+@Injectable()
+export class CustomHttp extends Http {
+    private activeCalls: number;
+    private store: Store<ApplicationState>;
+
+    constructor(backend: ConnectionBackend, defaultOptions: RequestOptions, store: Store<ApplicationState>) {
+        super(backend, defaultOptions);
+        this.store = store;
+        this.activeCalls = 0;
+    }
+
+    get(url: string, options?: RequestOptionsArgs): Observable<Response> {
+        this.httpCallRequested();
+
+        return super.get(url, options).finally(
+            () => {
+                this.httpCallReady();
+            }
+        );
+    }
+
+    request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
+        this.httpCallRequested();
+        return super.request(url, options).finally(
+            () => {
+                this.httpCallReady();
+            }
+        );
+    };
+
+    post(url: string, body: any, options?: RequestOptionsArgs): Observable<Response> {
+        this.httpCallRequested();
+        return super.post(url, body, options).finally(
+            () => {
+                this.httpCallReady();
+            }
+        );
+    };
+
+    put(url: string, body: any, options?: RequestOptionsArgs): Observable<Response> {
+        this.httpCallRequested();
+        return super.put(url, body, options).finally(
+            () => {
+                this.httpCallReady();
+            }
+        );
+    }
+
+    delete(url: string, options?: RequestOptionsArgs): Observable<Response> {
+        this.httpCallRequested();
+        return super.delete(url, options).finally(
+            () => {
+                this.httpCallReady();
+            }
+        );
+    }
+
+    patch(url: string, body: any, options?: RequestOptionsArgs): Observable<Response> {
+        this.httpCallRequested();
+        return super.patch(url, body, options).finally(
+            () => {
+                this.httpCallReady();
+            }
+        );
+    }
+
+    head(url: string, options?: RequestOptionsArgs): Observable<Response> {
+        this.httpCallRequested();
+        return super.head(url, options).finally(
+            () => {
+                this.httpCallReady();
+            }
+        );
+    }
+
+    /**
+     * Performs a request with `options` http method.
+     */
+    options(url: string, options?: RequestOptionsArgs): Observable<Response> {
+        this.httpCallRequested();
+        return super.options(url, options).finally(
+            () => {
+                this.httpCallReady();
+            }
+        );
+    }
+
+
+    private httpCallReady(): void {
+        this.activeCalls--;
+        if (this.activeCalls === 0) {
+            this.store.dispatch({type: CONTAINER_APPLICATION_DISABLE_BUSY_FLAG});
+        }
+    }
+
+    private httpCallRequested(): void {
+        this.activeCalls++;
+        if (this.activeCalls !== 0) {
+            this.store.dispatch({type: CONTAINER_APPLICATION_ENABLE_BUSY_FLAG});
+        }
+    }
+}
--- src/app/index.ts
+++ src/app/index.ts
@@ -7,19 +7,29 @@ import {routing} from "./routes";
 import {CommonLogicModule} from "../common/index";
 import {AuthenticationModule} from "../authentication/index";
 import {rootReducer} from "../statemanagement/rootReducer";
-import {StoreModule} from "@ngrx/store";
+import {StoreModule, Store} from "@ngrx/store";
 import {StoreLogMonitorModule, useLogMonitor} from "@ngrx/store-log-monitor";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {RequestOptions, XHRBackend, Http} from "@angular/http";
+import {CustomHttp} from "./customHttp";
+import {ApplicationState} from "../statemanagement/state/ApplicationState";
 @NgModule({
     imports: [BrowserModule, AboutModule, StockModule, routing, CommonLogicModule, AuthenticationModule, StoreModule.provideStore(rootReducer),
         StoreLogMonitorModule, StoreDevtoolsModule.instrumentStore({
-        monitor: useLogMonitor({
-            visible: false,
-            position: "right"
-        })
-    })],
+            monitor: useLogMonitor({
+                visible: false,
+                position: "right"
+            })
+        })],
     declarations: [ApplicationContainer],
-    bootstrap: [ApplicationContainer]
+    bootstrap: [ApplicationContainer],
+    providers: [{
+        provide: Http,
+        useFactory: (backend: XHRBackend,
+                     defaultOptions: RequestOptions,
+                     store: Store<ApplicationState>) => new CustomHttp(backend, defaultOptions, store),
+        deps: [XHRBackend, RequestOptions, Store]
+    }]
 })
 export class AppModule {
 }
