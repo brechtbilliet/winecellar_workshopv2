1497bd4c68d3e96e48bc4e8d110d43a85924f276
Brecht Billiet <billietbrecht@gmail.com>
2016-09-03 11:22:39 +0200

    used select of ngrx instead of subscribe

--- src/app/containers/application/application.container.ts
+++ src/app/containers/application/application.container.ts
@@ -1,39 +1,31 @@
 import {Title} from "@angular/platform-browser";
-import {Component, ViewEncapsulation, OnInit, OnDestroy} from "@angular/core";
+import {Component, ViewEncapsulation, OnInit} from "@angular/core";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "toastr/build/toastr.css";
 import "font-awesome/css/font-awesome.css";
-import {Account} from "../../../authentication/types/Account";
 import {AuthenticationService} from "../../../authentication/services/authentication.service";
 import {Router} from "@angular/router";
 import {ApplicationState} from "../../../statemanagement/state/ApplicationState";
 import {Store} from "@ngrx/store";
-import {Subscription} from "rxjs/Rx";
 @Component({
     selector: "application",
     encapsulation: ViewEncapsulation.None,
     styles: [require("./application.container.scss")],
     providers: [Title],
     template: `        
-        <navbar [account]="account" (logout)="onLogout()" *ngIf="isAuthenticated"></navbar>
+        <navbar [account]="account$|async" (logout)="onLogout()" *ngIf="isAuthenticated$|async"></navbar>
         <router-outlet></router-outlet>
         <spinner [spin]="isBusy"></spinner>
         <ngrx-store-log-monitor toggleCommand="ctrl-t" positionCommand="ctrl-m"></ngrx-store-log-monitor>
            `
 })
-export class ApplicationContainer implements OnInit, OnDestroy {
-    isAuthenticated: boolean;
-    account: Account;
+export class ApplicationContainer implements OnInit {
+    isAuthenticated$ = this.store.select(state => state.data.authentication.isAuthenticated);
+    account$ = this.store.select(state => state.data.authentication.account);
     isBusy: boolean = true;
 
-    private subscriptions: Array<Subscription> = [];
-
     constructor(private store: Store<ApplicationState>, private authenticationService: AuthenticationService, private router: Router){
-        this.subscriptions.push(this.store.subscribe((state: ApplicationState) => {
-            this.isAuthenticated = state.data.authentication.isAuthenticated;
-            this.account = state.data.authentication.account;
-        }));
     }
 
     onLogout(): void{
@@ -43,9 +35,5 @@ export class ApplicationContainer implements OnInit, OnDestroy {
 
     ngOnInit(): void {
         this.authenticationService.checkInitialAuthentication();
-    }
-
-    ngOnDestroy(): void {
-        this.subscriptions.forEach(sub => sub.unsubscribe());
     }
 }
