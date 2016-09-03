169e56ff296b2c1eee23900f8ba13c0214e98f94
Brecht Billiet <billietbrecht@gmail.com>
2016-09-03 10:21:52 +0200

    configuration.ts

---
+++ src/configuration.ts
@@ -0,0 +1,4 @@
+export const API_URL: string = "https://winecellarapp.herokuapp.com/api";
+export const LOCALSTORAGE_AUTH: string = "authentication";
+export const WINE_COM_API_URL: string = "http://services.wine.com/api/beta2/service.svc/json/";
+export const WINE_COM_API_KEY: string = "%API_KEY%";
