b242b6ff4c2ceccfe8b7cff262e37e953a376e8a
Brecht Billiet <billietbrecht@gmail.com>
2016-09-03 09:45:25 +0200

    empty reducers

--- src/statemanagement/reducers/data/wines.reducer.ts
+++ src/statemanagement/reducers/data/wines.reducer.ts
@@ -1,4 +1,5 @@
 import {Action} from "@ngrx/store";
+import {Wine} from "../../../stock/entities/Wine";
 export function winesReducer(state: Array<Wine> = [],
                              action: Action = null): Array<Wine> {
             return state;
