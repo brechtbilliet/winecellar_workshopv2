e678d25f5a8590ed100fbbb3f3ba95cf5b0bca84
Brecht Billiet <billietbrecht@gmail.com>
2016-09-03 11:14:28 +0200

    store devtools

--- src/app/index.ts
+++ src/app/index.ts
@@ -5,8 +5,18 @@ import {AboutModule} from "../about/index";
 import {StockModule} from "../stock/index";
 import {routing} from "./routes";
 import {CommonLogicModule} from "../common/index";
+import {StoreLogMonitorModule, useLogMonitor} from "@ngrx/store-log-monitor";
+import {rootReducer} from "../statemanagement/rootReducer";
+import {StoreModule} from "@ngrx/store";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 @NgModule({
-    imports: [BrowserModule, AboutModule, StockModule, routing, CommonLogicModule],
+    imports: [BrowserModule, AboutModule, StockModule, routing, CommonLogicModule, StoreModule.provideStore(rootReducer),
+        StoreLogMonitorModule, StoreDevtoolsModule.instrumentStore({
+        monitor: useLogMonitor({
+            visible: false,
+            position: "right"
+        })
+    })],
     declarations: [ApplicationContainer],
     bootstrap: [ApplicationContainer],
 })
